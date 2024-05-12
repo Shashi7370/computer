713a00aa506761a55c7f2b5d4e5a2b6d4bc790eb
Shashikant Kumar <102204713+Shashi7370@users.noreply.github.com>
2024-05-12 23:54:05 +0530

    Delete Python directory

--- Python/assitance/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-# srk_image = face_recognition.load_image_file("1.jpg")
-# srk_encoding = face_recognition.face_encodings(srk_image)[0]
--- Python/variable_in_Python.py
+++
@@ -1,9 +0,0 @@
-# Variable In Python
-
-a = 10
-b = 20
-print(a)
-print(a,10)
-print(id(a),id(b))
-c="hello"
-print(c)
